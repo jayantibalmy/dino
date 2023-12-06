6985857a2615a332622dc1650c78c38943306f4d
jbalmy <108647411+miniisokay@users.noreply.github.com>
2023-12-06 19:01:38 +0530

    Implement landscape mode for small screen

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Function to rotate to landscape mode in phones 
+function rotateToLandscapeMode() {
+    const viewportWidth = window.innerWidth;
+
+    if (viewportWidth < 600) {
+      // Adjust the threshold (600 in this example) based on your needs
+      document.body.classList.add('landscape');
+    } else {
+      document.body.classList.remove('landscape');
+    }
+  }
+  // Rotate to landscape mode initially
+  rotateToLandscapeMode();
+
+  // Attach the function to the window resize event
+  window.addEventListener('resize', rotateToLandscapeMode);
+
 const dino = document.querySelector(".dino");
 
 //variable to track whether the dino is currently jumping
@@ -63,3 +80,49 @@ window.addEventListener("keydown", (event) => {
         rightMove();
     }
 });
+
+function astFall() {
+    gsap.to(".right-ast-1", { y: 300, x: -300, duration: 1, repeat: -1 })
+    gsap.to(".right-ast-2", { y: 300, x: -300, duration: 0.5, repeat: -1 })
+    gsap.to(".right-ast-3", { y: 300, x: -300, rotation: 3246, duration: 1, repeat: -1 })
+    gsap.to(".left-ast-1", { y: 300, x: 300, duration: 1, repeat: -1 })
+    gsap.to(".left-ast-2", { y: 300, x: 300, duration: 0.5, repeat: -1 })
+    gsap.to(".left-ast-3", { y: 300, x: 300, rotation: 3246, duration: 1, repeat: -1 })
+}
+// const gameStart = true; 
+if (gameStart == true) {
+    astFall();
+}
+
+function checkCollision(dino, asteroid) {
+    const dinoRect = dino.getBoundingClientRect();
+    const asteroidRect = asteroid.getBoundingClientRect();
+    // Check for collision
+    if (
+        dinoRect.x < asteroidRect.x + asteroidRect.width &&
+        dinoRect.x + dinoRect.width > asteroidRect.x &&
+        dinoRect.y < asteroidRect.y + asteroidRect.height &&
+        dinoRect.y + dinoRect.height > asteroidRect.y
+    ) {
+        // Collision detected
+        handleCollision(); // Implement your collision behavior here
+    }
+}
+// function handleCollision() {
+//     // Implement the behavior when a collision occurs
+//     console.log('Collision detected!');
+//     // Add logic to end the game, reduce health, or trigger an animation
+// }
+// Example usage in your animation loop
+// function gameLoop() {
+//     // Get references to your dino and asteroids
+//     const dino = document.querySelector('.dino');
+//     const asteroid = document.querySelector('.ast-1'); // Change to your asteroid selector
+//     // Check for collision in each frame
+//     checkCollision(dino, asteroid);
+//     // Continue with your game loop logic
+//     // ...
+//     requestAnimationFrame(gameLoop);
+// }
+// Call the game loop to start the collision detection
+// gameLoop();
